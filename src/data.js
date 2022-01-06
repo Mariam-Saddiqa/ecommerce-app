2588ef01caca238e446ee0be6e40f2048fee4cff
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2022-01-06 21:56:19 +0100

    share

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Hoodie',
+            category:'Hoodie',
+            image:'/images/p1.jpg',
+            brand:'Nike',
+            rating:4,
+            numReview:8,
+            price:49,
+            description:'high quality product'
+
+        },
+        {
+            _id:'2',
+            name:'Jack&Jones Hoodie',
+            category:'Hoodie',
+            image:'/images/p1.jpg',
+            brand:'Jack&Jones',
+            rating:5,
+            numReview:6,
+            price:90,
+            description:'high quality product'
+
+        },
+        {
+            _id:'3',
+            name:'H&M Hoodie',
+            category:'Hoodie',
+            image:'/images/p1.jpg',
+            brand:'H&M',
+            rating:2.5,
+            numReview:10,
+            price:29,
+            description:'high quality product'
+
+        },
+        {
+            _id:'4',
+            name:'Zara Hoodie',
+            category:'Hoodie',
+            image:'/images/p1.jpg',
+            brand:'Zara',
+            rating:4.5,
+            numReview:5,
+            price:29,
+            description:'high quality product'
+
+        },
+        {
+            _id:'5',
+            name:'Adiddas Hoodie',
+            category:'Hoodie',
+            image:'/images/p1.jpg',
+            brand:'Adiddas',
+            rating:3.5,
+            numReview:7,
+            price:59,
+            description:'high quality product'
+
+        }
+    ]
+}
+
+export  default data;
